7fc70b8cfb6a80b1b7e4f9ec1ca847e59580caf0
Anton <50206467+makeappx@users.noreply.github.com>
2022-01-30 14:05:13 +0300

    Add files via upload

---
+++ RedCode/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace RedCode
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(254, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(250, 313);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "org 12\nDAT 1\nDAT 2\nDAT 3\nDAT 4\nDAT 5\nDAT 6\nDAT 7\nDAT 8\nDAT 9\nDAT 10\nDAT -11\nDAT 0" +
+    "\nadd $6,$-2\nmov $-3,$-2\nmod $6,$-3\njmz $-3,$-4\nmov $3,@-6\njmp $-5\ndat 1\ndat 0\nda" +
+    "t 2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Location = new System.Drawing.Point(12, 31);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(236, 308);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 347);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Шаг";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(254, 347);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Преобразовать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 352);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Текущая строка:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 352);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "0";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(427, 347);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Инфо";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(254, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Редактор кода";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Окно интерпретации";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 378);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.Text = "RedCode";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ RedCode/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace RedCode
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            Clear();
+        }
+        void Clear()
+        {
+            startidx = 0;
+            listBox1.Items.Clear();
+            for (int i = 0; i < 8000; i++)
+                listBox1.Items.Add(i.ToString().PadLeft(4, '0') + " DAT 0");
+            label2.Text = "0";
+        }
+        string pointers = "@$<>";
+        string[] instructions = new[] { "MOV", "CMP", "ADD", "SUB", "MUL", "DIV", "MOD", "JMP", "JMZ", "JMN" };
+        void Validate(string s)
+        {
+            var tmp = s.Split(',');
+            if (!pointers.Contains(tmp[0][0].ToString()) ||
+                !pointers.Contains(tmp[1][0].ToString()) ||
+                !int.TryParse(tmp[0].Substring(1), out _) ||
+                !int.TryParse(tmp[1].Substring(1), out _))
+                throw new Exception();
+        }
+        void Parse()
+        {
+            try
+            {
+                Clear();
+                int start = 0;
+                startidx = 0;
+                for (int i = 0; i < richTextBox1.Lines.Length; i++)
+                {
+                    var arr = richTextBox1.Lines[i].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (arr.Length == 0) continue;
+                    switch (arr[0].ToUpper())
+                    {
+                        case "ORG":
+                            startidx = int.Parse(arr[1]);
+                            break;
+                        case "DAT":
+                            {
+                                listBox1.Items[start] = start.ToString().PadLeft(4, '0') + " DAT";
+                                if (int.TryParse(arr[1], out int val))
+                                    listBox1.Items[start++] += " " + val;
+                                break;
+                            }
+                        case "JMP":
+                            {
+                                listBox1.Items[start] = start++.ToString().PadLeft(4, '0') + " JMP " + arr[1];
+                                if (!int.TryParse(arr[1].Substring(1), out int val) && !pointers.Contains(arr[1][0]))
+                                    throw new Exception();
+                                break;
+                            }
+                        default:
+                            if(!instructions.Contains(arr[0].ToUpper()))
+                                throw new Exception();
+                            else
+                            {
+                                listBox1.Items[start] = start++.ToString().PadLeft(4, '0') +
+                                    $" {instructions.First(x => x == arr[0].ToUpper())} " + arr[1];
+                                Validate(arr[1]);
+                                break;
+                            }
+                    }
+                }
+                label2.Text = startidx.ToString();
+                listBox1.SelectedIndex = startidx;
+            }
+            catch { Clear(); }
+        }
+        int startidx = 0;
+
+        int GetAddress(string cur, string addr)
+        {
+            int i = int.Parse(cur);
+            string[] kek; int temp;
+            switch(addr[0])
+            {
+                case '@':
+                    kek = ((string)listBox1.Items[i + int.Parse(addr.Substring(1))]).Split(' ');
+                    if (kek[2].Split(',').Length == 1)
+                        i = int.Parse(kek[0]) + int.Parse(kek[2].Substring(pointers.Contains(kek[2][0]) ? 1 : 0));
+                    else
+                        i = int.Parse(kek[0]) + int.Parse(kek[2].Split(',')[1].Substring(pointers.Contains(kek[2][0]) ? 1 : 0));
+                    break;
+                case '>':
+                    kek = ((string)listBox1.Items[i + int.Parse(addr.Substring(1))]).Split(' ');
+                    temp = int.Parse(kek[2].Substring(pointers.Contains(kek[2][0]) ? 1 : 0));
+                    listBox1.Items[i + int.Parse(addr.Substring(1))] =
+                       ((string)listBox1.Items[i + int.Parse(addr.Substring(1))])
+                       .Remove(((string)listBox1.Items[i + int.Parse(addr.Substring(1))]).LastIndexOf(' ') + (pointers.Contains(kek[2][0]) ? 1 : 0))
+                       + " " + (temp + 1).ToString();
+                    if (kek[2].Split(',').Length == 1)
+                        i = int.Parse(kek[0]) + int.Parse(kek[2].Substring(pointers.Contains(kek[2][0]) ? 1 : 0));
+                    else
+                        i = int.Parse(kek[0]) + int.Parse(kek[2].Split(',')[1].Substring(pointers.Contains(kek[2][0]) ? 1 : 0));
+                    break;
+                case '<':
+                    kek = ((string)listBox1.Items[i + int.Parse(addr.Substring(1))]).Split(' ');
+                    temp = int.Parse(kek[2].Substring(pointers.Contains(kek[2][0]) ? 1 : 0));
+                    listBox1.Items[i + int.Parse(addr.Substring(1))] =
+                     ((string)listBox1.Items[i + int.Parse(addr.Substring(1))])
+                     .Remove(((string)listBox1.Items[i + int.Parse(addr.Substring(1))]).LastIndexOf(' ') + (pointers.Contains(kek[2][0]) ? 1 : 0))
+                     + " " + (temp - 1).ToString();
+                    if (kek[2].Split(',').Length == 1)
+                        i = int.Parse(kek[0]) + int.Parse(kek[2].Substring(pointers.Contains(kek[2][0]) ? 1 : 0));
+                    else
+                        i = int.Parse(kek[0]) + int.Parse(kek[2].Split(',')[1].Substring(pointers.Contains(kek[2][0]) ? 1 : 0));
+                    break;
+                default:
+                    i += int.Parse(addr.Substring(1));
+                    break;
+            }
+            return (i + 8000) % 8000;
+        }
+
+        void NextStep()
+        {
+            var s = ((string)listBox1.Items[startidx]).Split(' ');
+            int x, y, temp, temp1, temp2, tempx;
+            if (s[1] == "DAT")
+                return;
+            else
+                switch(s[1])
+                {
+                    case "MOV":
+                        x = GetAddress(s[0], s[2].Split(',')[0]);
+                        y = GetAddress(s[0], s[2].Split(',')[1]);
+                        listBox1.Items[y] =
+                            ((string)listBox1.Items[y]).Split(' ')[0] +
+                            ((string)listBox1.Items[x]).Substring(4);
+                        break;
+                    case "ADD":
+                        x = GetAddress(s[0], s[2].Split(',')[0]);
+                        y = GetAddress(s[0], s[2].Split(',')[1]);
+                        temp = ((string)listBox1.Items[y]).IndexOf(',');
+                        temp1 = int.Parse(((string)listBox1.Items[y]).Substring(temp == -1 ? ((string)listBox1.Items[y]).LastIndexOf(' ') : temp + 2));
+                        tempx = ((string)listBox1.Items[x]).IndexOf(',');
+                        temp2 = int.Parse(((string)listBox1.Items[x]).Substring(tempx == -1 ? ((string)listBox1.Items[x]).LastIndexOf(' ') : tempx + 2));
+                        listBox1.Items[y] = ((string)listBox1.Items[y]).Remove(temp == -1 ? ((string)listBox1.Items[y]).LastIndexOf(' ') : temp + 1) + " ";
+                        listBox1.Items[y] += (temp2 + temp1).ToString();
+                        break;
+                    case "MUL":
+                        x = GetAddress(s[0], s[2].Split(',')[0]);
+                        y = GetAddress(s[0], s[2].Split(',')[1]);
+                        temp = ((string)listBox1.Items[y]).IndexOf(',');
+                        temp1 = int.Parse(((string)listBox1.Items[y]).Substring(temp == -1 ? ((string)listBox1.Items[y]).LastIndexOf(' ') : temp + 2));
+                        tempx = ((string)listBox1.Items[x]).IndexOf(',');
+                        temp2 = int.Parse(((string)listBox1.Items[x]).Substring(tempx == -1 ? ((string)listBox1.Items[x]).LastIndexOf(' ') : tempx + 2));
+                        listBox1.Items[y] = ((string)listBox1.Items[y]).Remove(temp == -1 ? ((string)listBox1.Items[y]).LastIndexOf(' ') : temp + 1) + " ";
+                        listBox1.Items[y] += (temp2 * temp1).ToString();
+                        break;
+                    case "SUB":
+                        x = GetAddress(s[0], s[2].Split(',')[0]);
+                        y = GetAddress(s[0], s[2].Split(',')[1]);
+                        temp = ((string)listBox1.Items[y]).IndexOf(',');
+                        temp1 = int.Parse(((string)listBox1.Items[y]).Substring(temp == -1 ? ((string)listBox1.Items[y]).LastIndexOf(' ') : temp + 2));
+                        tempx = ((string)listBox1.Items[x]).IndexOf(',');
+                        temp2 = int.Parse(((string)listBox1.Items[x]).Substring(tempx == -1 ? ((string)listBox1.Items[x]).LastIndexOf(' ') : tempx + 2));
+                        listBox1.Items[y] = ((string)listBox1.Items[y]).Remove(temp == -1 ? ((string)listBox1.Items[y]).LastIndexOf(' ') : temp + 1) + " ";
+                        listBox1.Items[y] += (temp1 - temp2).ToString();
+                        break;
+                    case "DIV":
+                        x = GetAddress(s[0], s[2].Split(',')[0]);
+                        y = GetAddress(s[0], s[2].Split(',')[1]);
+                        temp = ((string)listBox1.Items[y]).IndexOf(',');
+                        temp1 = int.Parse(((string)listBox1.Items[y]).Substring(temp == -1 ? ((string)listBox1.Items[y]).LastIndexOf(' ') : temp + 2));
+                        tempx = ((string)listBox1.Items[x]).IndexOf(',');
+                        temp2 = int.Parse(((string)listBox1.Items[x]).Substring(tempx == -1 ? ((string)listBox1.Items[x]).LastIndexOf(' ') : tempx + 2));
+                        listBox1.Items[y] = ((string)listBox1.Items[y]).Remove(temp == -1 ? ((string)listBox1.Items[y]).LastIndexOf(' ') : temp + 1) + " ";
+                        listBox1.Items[y] += (temp1 / temp2).ToString();
+                        break;
+                    case "MOD":
+                        x = GetAddress(s[0], s[2].Split(',')[0]);
+                        y = GetAddress(s[0], s[2].Split(',')[1]);
+                        temp = ((string)listBox1.Items[y]).IndexOf(',');
+                        temp1 = int.Parse(((string)listBox1.Items[y]).Substring(temp == -1 ? ((string)listBox1.Items[y]).LastIndexOf(' ') : temp + 2));
+                        tempx = ((string)listBox1.Items[x]).IndexOf(',');
+                        temp2 = int.Parse(((string)listBox1.Items[x]).Substring(tempx == -1 ? ((string)listBox1.Items[x]).LastIndexOf(' ') : tempx + 2));
+                        listBox1.Items[y] = ((string)listBox1.Items[y]).Remove(temp == -1 ? ((string)listBox1.Items[y]).LastIndexOf(' ') : temp + 1) + " ";
+                        listBox1.Items[y] += (temp1 % temp2).ToString();
+                        break;
+                    case "JMP":
+                        x = GetAddress(s[0], s[2]);
+                        startidx = x - 1;
+                        break;
+                    case "CMP":
+                        x = GetAddress(s[0], s[2].Split(',')[0]);
+                        y = GetAddress(s[0], s[2].Split(',')[1]);
+                        temp = ((string)listBox1.Items[y]).IndexOf(',');
+                        temp1 = int.Parse(((string)listBox1.Items[y]).Substring(temp == -1 ? ((string)listBox1.Items[y]).LastIndexOf(' ') : temp + 2));
+                        tempx = ((string)listBox1.Items[x]).IndexOf(',');
+                        temp2 = int.Parse(((string)listBox1.Items[x]).Substring(tempx == -1 ? ((string)listBox1.Items[x]).LastIndexOf(' ') : tempx + 2));
+                        if (temp1 == temp2) startidx++;
+                        break;
+                    case "JMZ":
+                        x = GetAddress(s[0], s[2].Split(',')[0]);
+                        y = GetAddress(s[0], s[2].Split(',')[1]);
+                        temp = ((string)listBox1.Items[y]).IndexOf(',');
+                        temp1 = int.Parse(((string)listBox1.Items[y]).Substring(temp == -1 ? ((string)listBox1.Items[y]).LastIndexOf(' ') : temp + 2));
+                        if (temp1 == 0) startidx = x - 1;
+                        break;
+                    case "JMN":
+                        x = GetAddress(s[0], s[2].Split(',')[0]);
+                        y = GetAddress(s[0], s[2].Split(',')[1]);
+                        temp = ((string)listBox1.Items[y]).IndexOf(',');
+                        temp1 = int.Parse(((string)listBox1.Items[y]).Substring(temp == -1 ? ((string)listBox1.Items[y]).LastIndexOf(' ') : temp + 2));
+                        if (temp1 != 0) startidx = x - 1;
+                        break;
+                }
+            startidx++;
+            label2.Text = startidx.ToString();
+            listBox1.SelectedIndex = startidx;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Parse();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NextStep();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Автор: Бурлаченко Антон Сергеевич\n" +
+                "4 Курс ИСТ\n" +
+                "Поддерживаемые команды: MOV, CMP, ADD, SUB, MUL, DIV, MOD, JMP, JMZ, JMN\n" +
+                "Поддерживаемые типы адресации: $, @, <, >");
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndex = startidx;
+        }
+    }
+}
